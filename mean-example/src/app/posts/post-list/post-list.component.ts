357db9d1454442a8fd95e85e8364ff411cbd2fb5
Su Mon Wai <sumon.wai@gecurrent.com>
2020-05-05 14:58:21 +1000

    Using subscription and Subject

--- mean-example/src/app/posts/post-list/post-list.component.ts
+++ mean-example/src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-post-list',
@@ -9,9 +10,24 @@ import { PostsService } from '../posts.service';
 })
 export class PostListComponent implements OnInit {
   @Input() posts: Post[] = [];
+  private postsSub: Subscription;
   constructor(public postsService: PostsService) {}
 
   ngOnInit(): void {
+    // Initialze to first start.
     this.posts = this.postsService.getPosts();
+
+    // Listener subscribe (functionCallWhenComplete)
+    // This live forever, use internal logic to
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+
+
+  }
+
+  // Cleanup memory
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
--- mean-example/src/app/posts/posts.service.ts
+++ mean-example/src/app/posts/posts.service.ts
@@ -1,15 +1,24 @@
 import { Post } from "./post.model";
 import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
 
+  // This is to send
+  private postUpdated = new Subject<Post[]>();
+
   getPosts() {
-    return this.posts;
+    return [...this.posts];
+  }
+
+  getPostUpdateListener() {
+    return this.postUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
     this.posts.push({title: title, content: content});
+    this.postUpdated.next([...this.posts]);
   }
 }
